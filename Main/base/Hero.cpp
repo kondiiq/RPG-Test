9114b105578ccad0e31d9ca817e543bf65d80013
kondiiq <249263@student.pwr.edu.pl>
2020-07-20 22:49:46 +0200

    Enter class Hero, next time try to solve problem with munmap_chunk()

---
+++ Main/base/Hero.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kondiiq on 20.07.2020.
+//
+
+#include "Hero.h"
---
+++ Main/base/Hero.h
@@ -0,0 +1,14 @@
+//
+// Created by kondiiq on 20.07.2020.
+//
+
+#ifndef BASE_HERO_H
+#define BASE_HERO_H
+
+
+class Hero {
+
+};
+
+
+#endif //BASE_HERO_H
